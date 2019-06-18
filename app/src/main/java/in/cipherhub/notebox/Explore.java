01cf754dc6d90e2f58e87465531361bdd25fc7fb
bivav <bivavraj@gmail.com>
2019-06-18 11:15:56 +0530

    Formatted Code

--- app/src/main/java/in/cipherhub/notebox/Explore.java
+++ app/src/main/java/in/cipherhub/notebox/Explore.java
@@ -6,7 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class explore extends Fragment {
+public class Explore extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
